899e8007913cfff8fa14251dc7db3c49f7170cbf
Gregory <Hoops1389@gmail.com>
2015-04-10 22:25:11 -0400

    Move Shenmue Page

---
+++ Dreamsite/public_html/PC/games/Shenmue/index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+
+<html>
+
+  <head>
+    <title>Shenmue</title>
+  </head>
+
+  <body link="#0022EE" vlink="#0022EE" alink="red" text="black" bgcolor="blue" background="../../images/tile.gif">
+
+    <font face="Helvetica">
+      <table cellpadding="2" cellspacing="0" border="1" width="640" align="center" bgcolor="#FFFFFF">
+
+        <!-- Main Title Image -->
+        <tr bgcolor="#CCCCCC">
+          <th align="center"><img src="../../images/title.png" alt="" align="center" width="640" height="160"></th>
+        </tr>
+
+        <!-- Header Links -->
+        <tr bgcolor="#CCCCCC">
+          <th align="center">
+            <table width="100%"><tr>
+              <th><a href="../../index.html" style="text-decoration:none">Home</a></th>
+              <th><a href="../index.html" style="text-decoration:none">Games</a></th>
+              <th><a href="../../vmu.html" style="text-decoration:none">VMU</a></th>
+              <th><a href="../../links.html" style="text-decoration:none">Links</a></th>
+              <th><a href="../../thanks.html" style="text-decoration:none">Thanks</a></th>
+            </tr></table>
+          </th>
+        </tr>
+
+        <!-- Body -->
+        <tr bgcolor="#FFFFFF"><td align="center" background="../../images/tile2.png">
+
+          <h1 align="left">Shenmue</h1>
+
+          <table cellpadding="3" cellspacing="1" border"0">
+            <tr>
+              <td>
+                <table cellpadding="3" cellspacing="1" border="0" width="150" bgcolor="#6E6E6E">
+                  <tr align="center" bgcolor="#BBBBBB">
+                    <td><b>Contents</b></td>
+                  </tr>
+                  <tr align="center" bgcolor="#DDDDDD">
+                    <td>Files</td>
+                  </tr>
+                  <tr bgcolor="#CEEBF5">
+                    <td><a href="#save">Game Saves</a></td>
+                  </tr>
+                  <tr align="center" bgcolor="#DDDDDD">
+                    <td>Websites</td>
+                  </tr>
+                  <tr bgcolor="#FFFFFF">
+                    <td><a href="#gweb">Passport</a></td>
+                  </tr>
+                </table>
+              </td>
+              <td>
+                <label>
+                  <br>
+                  Here is some quick information about this game. Be sure to
+                  push the content of this paragraph over the limit of a single
+                  line or it will look funny.
+                  <br><br>
+                </label>
+              </td>
+            </tr>
+          </table>
+
+          <hr>
+
+          <p><h3 align="left"><u><a id="save">Game Saves</a></u></h3>
+            <p align="left">There is only one save file for this game which holds 3 save slots.
+              </p>
+            <table align="center" cellpadding="3" cellspacing="1" border="0" width="90%" bgcolor="#6E6E6E">
+              <tr bgcolor="#CCCCCC">
+                <th width="100px">Name</th><th width="65px">Size<br>(Blocks)</th><th>Description</th><th width="34px">Icon</th>
+              </tr>
+
+              <tr bgcolor="#CEEBF5">
+                <td align="center"><a href="saves_file.html" style="text-decoration:none">Main Data</a></td>
+                <td align="center">80</td>
+                <td>Save file for the main game data.</td>
+                <td align="center"><img src="images/saves/GAME.gif"></td>
+              </tr>
+            </table>
+          </p>
+
+          <hr>
+
+          <p><h3 align="left"><u><a id="gweb">In-Game Websites</a></u></h3>
+            <p align="left">These are websites that are accessable through
+            the Passport included as Disk 4.
+            <table align="center" cellpadding="3" cellspacing="1" border="0" style="min-width:600px;max-width:640px;" bgcolor="#6E6E6E">
+              <tr>
+                <th align="center" bgcolor="#CCCCCC">Site</th><th align="center" bgcolor="#CCCCCC">URL</th><th align="center" bgcolor="#CCCCCC">Link</th>
+              </tr>
+              <tr bgcolor="#A6FFB2">
+                <td>USA Site</td>
+                <td align="right"><code>passport.shenmue.com</code></td>
+                <td align="center"><code><a href="passport.shenmue.com">Link</a></code></td>
+              </tr>
+            </table>
+          </p>
+        </tr>
+        <tr bgcolor="#CCCCCC">
+          <td align="center">
+            <font size="1">
+              <br>
+              Last modified: 2015-2-6 9:54 PM EST<br>
+              Design Copyright &copy; 2015 Gregory Hoople<br>
+              <br>
+            </font>
+          </td>
+        </tr>
+      </table>
+    </font>
+  </body>
+</html>
